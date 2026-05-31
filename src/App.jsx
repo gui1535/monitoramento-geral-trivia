a9baa7abb3196bdfcf9231fb1749cc3830eb058f
Guilherme <guilherme.araujo1535@gmail.com>
2026-05-31 16:07:48 -0300

    Correção apenas 1 pagina para monitoramento

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
 import { GlobalStyles } from './components/GlobalStyles'
-import { LoginPage } from './pages/LoginPage'
 import { MonitoramentoPage } from './pages/MonitoramentoPage'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/monitoramento" element={<MonitoramentoPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <MonitoramentoPage />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-
-const routerBasename =
-  import.meta.env.BASE_URL === '/'
-    ? undefined
-    : import.meta.env.BASE_URL.replace(/\/$/, '')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={routerBasename}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
